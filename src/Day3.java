1571ff27f79ce487867cedab63fbb0ff85cde00f
Richard Davidsson <richard.davidsson@yh.nackademin.se>
2023-12-03 20:40:37 +0100

    day 3 part 1 done

--- src/Day3.java
+++ src/Day3.java
@@ -1,28 +1,14 @@
-
-/*
-Part 1
-. is not a symbol
-if number is adjacent to a symbol add number to total
-
-how: maybe pick out all special chars and then look for their index in line
-then check this, previous and nextLine at those index as well as index -1 and index +1 ?
-
-
-*/
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Day3 {
-    List<String> specialChars = List.of("-", "%", "+", "=", "*", "/", "$", "&", "#", "@");
-
     public static void main(String[] args) throws FileNotFoundException {
         Day3 day3 = new Day3();
         day3.partOne();
     }
-
-
     private void partOne() throws FileNotFoundException {
         File file = new File("src/Inputs/Day3-1.txt");
         Scanner scanner = new Scanner(file);
@@ -33,35 +19,30 @@ public class Day3 {
         int total = 0;
         for (int i = 0; i <= lines.size() - 1; i++) {
             String currentLine = lines.get(i);
-            System.out.println(i+1);
             String previousLine = i == 0 ? lines.get(i) : lines.get(i - 1);
             String nextLine = i == lines.size() - 1 ? lines.get(i) : lines.get(i + 1);
             List<String> nums = getNumbersInLine(currentLine);
             int x = 0;
             if (!nums.isEmpty()) {
-                List<String> usedNums = new ArrayList<>();
                 for (String num : nums) {
-                    int startIndex = usedNums.contains(num) ? currentLine.lastIndexOf(num) :currentLine.indexOf(num);
-                    int endIndex = usedNums.contains(num) ? currentLine.lastIndexOf(num) + num.length() : currentLine.indexOf(num) + num.length();
-                    String s1 = currentLine.substring(startIndex - 1, endIndex + 1);
-                    String s2 = previousLine.substring(startIndex - 1, endIndex + 1);
-                    String s3 = nextLine.substring(startIndex - 1, endIndex + 1);
-                    String checkerString = s1+s2+s3;
-                    for (String s: specialChars){
-                        if (checkerString.contains(s)){
-                            //System.out.println("Num has adjacent symbol " + num);
-                            x += Integer.parseInt(num);
-                        }
+                    int startIndex = currentLine.indexOf(num);
+                    int endIndex = currentLine.indexOf(num) + num.length();
+                    String s1 = currentLine.substring(startIndex - 1, endIndex + 1).replaceAll("[0-9]", "");
+                    String s2 = previousLine.substring(startIndex - 1, endIndex + 1).replaceAll("[0-9]", "");
+                    String s3 = nextLine.substring(startIndex - 1, endIndex + 1).replaceAll("[0-9]", "");
+                    String checkerString = (s1 + s2 + s3).replaceAll("\\.", "");
+                    if (!checkerString.isEmpty()) {
+                        x += Integer.parseInt(num);
                     }
-                    usedNums.add(num);
+                    currentLine = currentLine.substring(endIndex);
+                    previousLine = previousLine.substring(endIndex);
+                    nextLine = nextLine.substring(endIndex);
                 }
-                System.out.println("Line Total: " +x);
                 total += x;
             }
         }
         System.out.println(total);
     }
-
     private List<String> getNumbersInLine(String line) {
         List<String> nums = new ArrayList<>();
         String y = line.replaceAll("[-%+=*/$&#@]", ".");
@@ -76,4 +57,5 @@ public class Day3 {
         }
         return nums;
     }
+
 }
